93d4812f54c89cb76949b53840d3dedc9a905a70
David Beskow <dmbeskow@users.noreply.github.com>
2022-07-21 22:59:22 -0400

    Update kmeans_sim.R
    Added comments and install packages automatically

--- kmeans_sim.R
+++ kmeans_sim.R
@@ -1,6 +1,8 @@
-library(tidymodels)
-library(Rfast)
+# this installs and loads packages
+if (!require("pacman")) install.packages("pacman")
+pacman::p_load(tidymodels, Rfast)
 
+# Create our dataset of three clusters
 set.seed(27)
 
 centers <- tibble(
@@ -19,69 +21,79 @@ points <-
   select(-num_points) %>% 
   unnest(cols = c(x1, x2))
 
+# Plot initial visualization
 p <- ggplot(points, aes(x1, x2)) +
   geom_point(alpha = 0.3, size = 2) + theme_classic() + ggtitle("Original Data")
 
 print(p)
 
+# Pause for user to view
 Sys.sleep(3)
 
+# Randomly sample points to identify initial centroids
 points$centroid <- 1
 points$centroid[sample(1:nrow(points),3)] <-2
-
 centroids <- points %>% filter(centroid == 2) %>% select(x1, x2)
 
+# Plot initial centroids
 p <- ggplot(points, aes(x1, x2, size = centroid, shape = factor(centroid), color = factor(centroid)) ) +
   geom_point(alpha = 0.3) + theme_classic() + ggtitle("Identify Initial Centroids") + 
   scale_size(range = c(2,4)) + theme(legend.position="none") + 
   scale_color_manual(values=c("#999999", "red"))
 print(p)
 
+# Pause for user to view
 Sys.sleep(3)
   
 converged = FALSE
 count <- 1
 
+# While algorithm has not converged (centroids stop changing)
 while(!converged){
   
   print(paste("Iteration", count))
   
+  # Compute distance matrix
   mat <- as.matrix(points[,c('x1','x2')])
-  
   d <- dista(mat, centroids[,c('x1','x2')])
+  
+  # Use min distance to identify clusters
   clust <- apply(d,1, which.min)
   
   points$cluster <- clust
   
+  # Visualize assignment
   p <- ggplot(points, aes(x1, x2, size = centroid, shape = factor(centroid), color = factor(cluster))) +
     geom_point(alpha = 0.3) + theme_classic() + ggtitle("Assign Points to Cluster") + 
     scale_size(range = c(2,4)) + theme(legend.position="none")
   
   print(p)
   
+  # Pause for user to view
   Sys.sleep(3)
   
   # Recalculate centroids
-  
   new_centroids <- points  %>% group_by(cluster) %>% 
     summarise(x1 = mean(x1), x2 = mean(x2)) %>% mutate(centroid = 2) 
   
+  # Check for convergence
   if(identical(centroids, new_centroids)){
     break
   } else{
     centroids = new_centroids
   }
   
-  
+  # Visualize new centroids
   points <- points %>% mutate(centroid = 1) %>% bind_rows(centroids)
   
   gg <- ggplot(points, aes(x1, x2, size = centroid, shape = factor(centroid), color = factor(cluster))) +
     geom_point(alpha = 0.3) + theme_classic() + ggtitle("Adjust Centroids to Mean of Cluster") + 
     scale_size(range = c(2,4)) + theme(legend.position="none")
-  
   print(gg)
   
+  # Pause for user to view
   Sys.sleep(3)
+  
   count <- count + 1
   
 }
